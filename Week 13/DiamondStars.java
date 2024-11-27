4db421d21c986439ecde13d52bc456efa2d6e361
GitRafi <rafiraff2005@gmail.com>
2024-11-27 19:23:10 +0700

    java-semester1

--- Week 13/DiamondStars.java
+++ Week 13/DiamondStars.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 public class DiamondStars {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Inputkan angka positif: ");
-        int a = sc.nextInt();
-        assert a > 0 : "Bilangan positif kocak";
-
-        for (int i=1;i<=a;i++) {
-            for (int j=1;j<=i;j++) {
+        System.out.print("Inputkan angka positif: ");
+        int n = sc.nextInt();
+        String space = " ";
+        int x = n-1;
+        int y = 0;
+        int m = 1;
+        int l = n * 2 - 1;
+        assert n > 0 : "Bilangan positif bang -_-";
+        
+        for (int i=1;i<=n;i++) {
+            System.out.print(space.repeat(x-y));
+            for (int j=0;j<m;j++) {
                 System.out.print("*");
             }
             System.out.println();
+            m+=2;
+            y++;
         }
+        for (int i=1;i<n;i++) {
+            System.out.print(space.repeat(i));
+            for (int j=1;j<=(l-2);j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            l-=2;
+        }
+        sc.close();
     }
 }
